fb776188b58e760f4c02dfe59f5568e650386308
Mandar <devwithmandar@gmail.com>
2024-08-16 21:48:37 -0400

    rag with llm

--- 3_rag/rag_with_llm.py
+++ 3_rag/rag_with_llm.py
@@ -4,7 +4,8 @@ import os
 from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import CharacterTextSplitter
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(cur_dir, "books", "langchain_demo.txt")
@@ -56,14 +57,33 @@ retriever = db.as_retriever(
 
 relevant_docs = retriever.invoke(query)
 
-print("\n--- Relevant Documents ---")
-for i, doc in enumerate(relevant_docs, 1):
-    print(f"Document {i}:\n{doc.page_content}\n")
-    if doc.metadata:
-        print(f"Source: {doc.metadata.get('source', 'Unknown')}\n")
+# print("\n--- Relevant Documents ---")
+# for i, doc in enumerate(relevant_docs, 1):
+#     print(f"Document {i}:\n{doc.page_content}\n")
+#     if doc.metadata:
+#         print(f"Source: {doc.metadata.get('source', 'Unknown')}\n")
 
 
-    
+combined_input = (
+    "Here are some documents that might help answer the questions: "
+    + query
+    + "\n\nRelevant Documents:\n"
+    + "\n\n".join([doc.page_content for doc in relevant_docs])
+    + "\n\nPlease provide an answer based only on provided documents. If answer is not found in documents, respond with I'm not sure."
+
+)
+
+model = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+
+messages = [
+    SystemMessage(content="You are helpful assistant"),
+    HumanMessage(content=combined_input)
+
+]
+
+result = model.invoke(messages)
+
+print(result)
 
 
 
